f2498259db23692d10cd4d7cc86b7af63c799032
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:59 -0300

    chore: re index choosenimgui

--- flake.nix
+++ flake.nix
@@ -7,22 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."choosenimgui-master".dir   = "master";
-  inputs."choosenimgui-master".owner = "nim-nix-pkgs";
-  inputs."choosenimgui-master".ref   = "master";
-  inputs."choosenimgui-master".repo  = "choosenimgui";
-  inputs."choosenimgui-master".type  = "github";
-  inputs."choosenimgui-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."choosenimgui-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."choosenimgui-v0_1_0".dir   = "v0_1_0";
-  inputs."choosenimgui-v0_1_0".owner = "nim-nix-pkgs";
-  inputs."choosenimgui-v0_1_0".ref   = "master";
-  inputs."choosenimgui-v0_1_0".repo  = "choosenimgui";
-  inputs."choosenimgui-v0_1_0".type  = "github";
-  inputs."choosenimgui-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."choosenimgui-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
